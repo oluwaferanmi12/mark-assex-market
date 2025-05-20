3c466c9b212cb90cba8e9b9eb601e1cc077612c0
Mark <feranmijogbodo@gmail.com>
2025-05-20 23:21:50 +0100

    deposit page

--- src/app/(dashboard)/deposit/page.tsx
+++ src/app/(dashboard)/deposit/page.tsx
@@ -1,9 +1,142 @@
-import React from 'react'
+"use client";
+
+import { DepositWrapper } from "@/components/shared/wrappers/deposit-wrapper";
+import { DropDownTextWrapper } from "@/components/shared/wrappers/drop-down-text";
+import { DropDownList } from "@/components/ui/drop-down/dropdown-list";
+import { PageHeader } from "@/components/ui/text/page-header";
+import { DropDownListInterface } from "@/interfaces/ui-interfac";
+import { Col, Row } from "antd";
+import React, { useState } from "react";
+import koraHq from "@/assets/svgs/korahq.svg";
+import paystack from "@/assets/svgs/paystack.svg";
+import crypto from "@/assets/svgs/deposit-crypto.svg";
+import cautionIcon from "@/assets/svgs/caution-icon.svg";
+import Image from "next/image";
 
 const Deposit = () => {
-  return (
-    <div>Deposit</div>
-  )
-}
+  const [statusSelected, setStatusSelected] = useState("All");
+  const [activeIndex, setActiveIndex] = useState(0);
+  const statusDropDownList: DropDownListInterface[] = [
+    { text: "All", id: "" },
+    { text: "Successful", id: "" },
+    { text: "Pending", id: "" },
+    { text: "Failed", id: "" },
+  ];
 
-export default Deposit
+  const depositObject = [
+    { text: "Crypto Chilll", icon: crypto },
+    { text: "Korahq", icon: koraHq },
+    { text: "Paystack", icon: paystack },
+  ];
+
+  return (
+    <>
+      <PageHeader text="Deposit" />
+      <div className="mt-4">
+        <Row>
+          <Col xs={4}>
+            <p className="text-[#707070] text-xs font-work-sans-regular mb-1">
+              My Account
+            </p>
+            <DropDownList
+              dropDownList={statusDropDownList}
+              selected={statusSelected}
+              setSelected={setStatusSelected}
+            >
+              <div>
+                <DropDownTextWrapper filterSelected={statusSelected} />
+              </div>
+            </DropDownList>
+          </Col>
+        </Row>
+      </div>
+      <div className="mt-8 flex gap-4">
+        {depositObject.map((item, index) => {
+          return (
+            <DepositWrapper
+              setActiveIndex={setActiveIndex}
+              index={index}
+              text={item.text}
+              active={index === activeIndex}
+              icon={item.icon}
+            />
+          );
+        })}
+      </div>
+      <div className="bg-white rounded-lg mt-6 p-4">
+        <div className="bg-[#EE95071A] p-4 flex items-center gap-3 rounded-lg">
+          <Image src={cautionIcon} alt="" />
+          <div>
+            <span className="font-work-sans-medium text-sm">
+              Important Information:{" "}
+            </span>
+            <span className="font-work-sans-regular text-sm">
+              Depositing with Crypto Chill? Here’s What You Need to Know
+            </span>
+          </div>
+        </div>
+        <div className="my-6 text-base">
+          <span className="font-work-sans-medium">Note:</span>{" "}
+          <span className="font-work-sans-regular">
+            AssexMarkets Global Limited does not impose or charge any fees for
+            deposits. However, you are solely responsible for any applicable
+            network or blockchain processing fees.
+          </span>
+        </div>
+        <div className="mb-4 text-base">
+          <p className="font-work-sans-medium">
+            1. Use the Correct Wallet Address:
+          </p>
+          <p className="font-work-sans-regular">
+            {`Always double-check that you’re using the correct wallet address. Sending funds to an incorrect address may result in permanent loss.`}
+          </p>
+        </div>
+        <div className="mb-4 text-base">
+          <p className="font-work-sans-medium">
+            2. Ensure Network Compatibility:
+          </p>
+          <p className="font-work-sans-regular">
+            {`Only send Crypto from platforms or wallets
+      that support Crypto transactions. Unsupported networks may lead to failed
+      or lost deposits`}
+          </p>
+        </div>
+        <div className="mb-4 text-base">
+            <p className="font-work-sans-medium">
+              3. Verify the Minimum Deposit Amount:
+            </p>
+          <p className="font-work-sans-regular">
+            {`Make sure your deposit meets the
+      platform’s required minimum. Some exchanges may enforce specific limits
+      for Crypto deposits.`}
+          </p>
+        </div>
+
+        <div className="mb-4 text-base">
+          <p className="font-work-sans-medium">
+            4. Track Transaction Confirmations:
+          </p>
+          <p className="font-work-sans-regular">
+            {`After initiating your deposit, monitor
+      the blockchain for confirmation. Confirmation times can vary based on
+      network activity and congestion.`}
+          </p>
+        </div>
+
+        <div className="mb-4 text-base">
+          <p className="font-work-sans-medium">
+            5. Keep a Record of Your Transaction:
+          </p>
+          <p className="font-work-sans-regular">
+            {`Save details such as the wallet
+      address, transaction ID, amount, and timestamp. These records are vital
+      for tracking your deposit and resolving any issues that may arise.`}
+          </p>
+        </div>
+      </div>
+      
+    </>
+  );
+};
+
+export default Deposit;
---
+++ src/components/shared/wrappers/deposit-wrapper.tsx
@@ -0,0 +1,73 @@
+"use client";
+import radioChecked from "@/assets/svgs/radio-button-filled.svg";
+import radioUnChecked from "@/assets/svgs/radio-button.svg";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { useState } from "react";
+
+export const DepositWrapper = ({
+  icon,
+  active,
+  text,
+  index,
+  setActiveIndex,
+}: {
+  icon: string;
+  active: boolean;
+  text: string;
+  index: number;
+  setActiveIndex: (val: number) => void;
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <div className="relative w-full">
+      <div
+        onClick={() => {
+          setActiveIndex(index);
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        className={`${
+          active
+            ? "border-l-2 border-b-2 border-[#0DAE9480]"
+            : "border-[#BEBEBE59] border"
+        }  w-full py-6  bg-white rounded-2xl p-4 flex items-center gap-2 cursor-pointer `}
+      >
+        <Image src={active ? radioChecked : radioUnChecked} alt="" />
+        <Image src={icon} alt="" />
+        <p className="text-base font-work-sans-regular">{text}</p>
+      </div>
+
+      {isHovered && (
+        <motion.div
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 10 }}
+          transition={{ duration: 0.3 }}
+          className="bg-[#FFFFFF] w-full p-8 absolute -bottom-40 mt-2 rounded-lg shadow-md"
+        >
+          <div className="flex items-center gap-8 mb-3">
+            <p className="text-[#404040] font-work-sans-regular">
+              Min Deposit:
+            </p>
+            <p className="font-work-sans-regular text-[#111111]">$10.00</p>
+          </div>
+          <div className="flex items-center gap-8 mb-3">
+            <p className="text-[#404040] font-work-sans-regular">
+              Max Deposit:
+            </p>
+            <p className="font-work-sans-regular text-[#111111]">
+              $50,000,000.00
+            </p>
+          </div>
+          <div className="flex items-center gap-8 mb-3">
+            <p className="text-[#404040] font-work-sans-regular">Commission:</p>
+            <p className="font-work-sans-regular text-[#111111]">
+              From $0.00 to $01.00
+            </p>
+          </div>
+        </motion.div>
+      )}
+    </div>
+  );
+};
