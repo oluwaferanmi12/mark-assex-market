f1212e77a04557f694bd78adaeff0e815415cc02
Mark <feranmijogbodo@gmail.com>
2025-06-02 20:35:52 +0100

    added new verification page

--- src/app/(dashboard)/deposit/page.tsx
+++ src/app/(dashboard)/deposit/page.tsx
@@ -18,6 +18,7 @@ import dollarGreen from "@/assets/svgs/dollar-green.svg";
 import { ModalContainer } from "@/components/shared/modal-wrapper/modal-wrapper";
 import bigXIcon from "@/assets/svgs/moda-big-x-icon.svg";
 import { VisibleOnDesktop } from "@/components/shared/wrappers/visible-on-desktop";
+import internalTransferIcon from "@/assets/svgs/internal-transfer-icon.svg";
 
 const Deposit = () => {
   const [statusSelected, setStatusSelected] = useState("All");
@@ -32,6 +33,7 @@ const Deposit = () => {
   ];
 
   const depositObject = [
+    {text: "Internal Transfer" , icon: internalTransferIcon},
     { text: "Crypto Chilll", icon: crypto },
     { text: "Korahq", icon: koraHq },
     { text: "Paystack", icon: paystack },
@@ -84,217 +86,22 @@ const Deposit = () => {
           </Col>
         </Row>
       </div>
-      <div className="mt-8 flex flex-col lg:flex-row gap-4">
+      <Row gutter={16} className="mt-8">
         {depositObject.map((item, index) => {
           return (
-            <DepositWrapper
-              setActiveIndex={setActiveIndex}
-              index={index}
-              text={item.text}
-              active={index === activeIndex}
-              icon={item.icon}
-            />
+            <Col xs={24} lg={12} className="mb-4">
+              <DepositWrapper
+                setActiveIndex={setActiveIndex}
+                index={index}
+                text={item.text}
+                active={index === activeIndex}
+                icon={item.icon}
+              />
+            </Col>
           );
         })}
-      </div>
-      {!showForm ? (
-        <div>
-          <div className="bg-white rounded-lg mt-6 p-4">
-            <div className="bg-[#EE95071A] p-4 flex items-center gap-3 rounded-lg">
-              <Image src={cautionIcon} alt="" />
-              <div>
-                <span className="font-work-sans-medium text-sm">
-                  Important Information:{" "}
-                </span>
-                <span className="font-work-sans-regular text-sm">
-                  Depositing with Crypto Chill? Here’s What You Need to Know
-                </span>
-              </div>
-            </div>
-            <div className="my-6 text-base">
-              <span className="font-work-sans-medium">Note:</span>{" "}
-              <span className="font-work-sans-regular">
-                AssexMarkets Global Limited does not impose or charge any fees
-                for deposits. However, you are solely responsible for any
-                applicable network or blockchain processing fees.
-              </span>
-            </div>
-            <div className="mb-4 text-base">
-              <p className="font-work-sans-medium">
-                1. Use the Correct Wallet Address:
-              </p>
-              <p className="font-work-sans-regular">
-                {`Always double-check that you’re using the correct wallet address. Sending funds to an incorrect address may result in permanent loss.`}
-              </p>
-            </div>
-            <div className="mb-4 text-base">
-              <p className="font-work-sans-medium">
-                {"2. "}Ensure Network Compatibility:
-              </p>
-              <p className="font-work-sans-regular">
-                {`Only send Crypto from platforms or wallets
-      that support Crypto transactions. Unsupported networks may lead to failed
-      or lost deposits`}
-              </p>
-            </div>
-            <div className="mb-4 text-base">
-              <p className="font-work-sans-medium">
-                3. Verify the Minimum Deposit Amount:
-              </p>
-              <p className="font-work-sans-regular">
-                {`Make sure your deposit meets the
-      platform’s required minimum. Some exchanges may enforce specific limits
-      for Crypto deposits.`}
-              </p>
-            </div>
-
-            <div className="mb-4 text-base">
-              <p className="font-work-sans-medium">
-                4. Track Transaction Confirmations:
-              </p>
-              <p className="font-work-sans-regular">
-                After initiating your deposit, monitor the blockchain for
-                confirmation. Confirmation times can vary based on network
-                activity and congestion.
-              </p>
-            </div>
-
-            <div className="mb-4 text-base">
-              <p className="font-work-sans-medium">
-                5. Keep a Record of Your Transaction:
-              </p>
-              <p className="font-work-sans-regular">
-                {`Save details such as the wallet
-      address, transaction ID, amount, and timestamp. These records are vital
-      for tracking your deposit and resolving any issues that may arise.`}
-              </p>
-            </div>
-          </div>
-
-          <div className="mt-3">
-            <Button
-              iconPosition="right"
-              icon={arrowRightMultiple}
-              loading={false}
-              action={() => {
-                setShowForm(true);
-              }}
-              text="Proceed"
-              variant="green-bg"
-            />
-          </div>
-        </div>
-      ) : (
-        <>
-          <div className="bg-white rounded-lg p-4 mt-6">
-            <div>
-              <Row className="mb-4">
-                <Col lg={12} xs={24}>
-                  <div>
-                    <p className="text-[#707070] text-sm font-work-sans-regular mb-1">
-                      Currency
-                    </p>
-                    <select
-                      style={{
-                        boxShadow: "0px 2px 5px 0px rgba(68, 68, 68, 0.1)",
-                      }}
-                      className="w-full p-4 focus:outline-none rounded-lg text-[#707070] font-work-sans-regular"
-                    >
-                      <option>Select Currency</option>
-                    </select>
-                  </div>
-                </Col>
-              </Row>
-              <Row>
-                <Col xs={24} lg={12}>
-                  <div>
-                    <p className="text-[#707070] text-sm font-work-sans-regular mb-1">
-                      Amount
-                    </p>
-                    <div>
-                      <span className="absolute flex items-center justify-center top-8 right-4 bg-[#E7F7F4] rounded-lg border border-#0DAE94[] py-1 px-2">
-                        <Image src={dollarGreen} alt="" />
-                      </span>
-
-                      <input
-                        placeholder="Enter amount"
-                        style={{
-                          boxShadow: "0px 2px 5px 0px rgba(68, 68, 68, 0.1)",
-                        }}
-                        className="w-full p-4 focus:outline-none rounded-lg text-[#707070] font-work-sans-regular"
-                      />
-                    </div>
-                  </div>
-                </Col>
-              </Row>
-              <div className="my-4">
-                <Button
-                  action={() => {
-                    setVerificationModal(true);
-                  }}
-                  loading={false}
-                  text="Proceed"
-                  variant="green-bg"
-                  icon={arrowRightMultiple}
-                  iconPosition="right"
-                />
-              </div>
-              <VisibleOnDesktop>
-                <div className="bg-white border border-[#BEBEBE80] p-4 rounded-sm">
-                  <Row>
-                    <Col xs={12}>
-                      <div className="flex items-center gap-4 mb-4 text-lg font-work-sans-regular">
-                        <p className="text-[#404040]">Min Deposit:</p>
-                        <p className="text-[#111111]">$10.00</p>
-                      </div>
-                      <div className="flex items-center gap-4 mb-4 text-lg font-work-sans-regular">
-                        <p className="text-[#404040]">Max Deposit:</p>
-                        <p className="text-[#111111]">$50,000,000.00</p>
-                      </div>
-                    </Col>
-                    <Col xs={12}>
-                      <div className="flex items-center gap-4 mb-4 text-lg font-work-sans-regular">
-                        <p className="text-[#404040]">Commission:</p>
-                        <p className="text-[#111111]">From $0.00 to $01.00</p>
-                      </div>
-                      <div className="flex items-center gap-4 mb-4 text-lg font-work-sans-regular">
-                        <p className="text-[#404040]">Deposit Time:</p>
-                        <p className="text-[#111111]">Instant</p>
-                      </div>
-                    </Col>
-                  </Row>
-                </div>
-              </VisibleOnDesktop>
-            </div>
-          </div>
-          <div className="bg-white rounded-2xl border border-[#BEBEBE80] p-4 mt-4">
-            <div className="flex items-center gap-4 mb-4">
-              <p className="text-[#404040] font-work-sans-regular text-xs">
-                Min deposit:
-              </p>
-              <p className="text-xs text-[#111111]">$10.00</p>
-            </div>
-            <div className="flex items-center gap-4 mb-4">
-              <p className="text-[#404040] font-work-sans-regular text-xs">
-                Commission:
-              </p>
-              <p className="text-xs text-[#111111]">From $0.00 to $01.00</p>
-            </div>
-            <div className="flex items-center gap-4 mb-4">
-              <p className="text-[#404040] font-work-sans-regular text-xs">
-                Max Deposit:
-              </p>
-              <p className="text-xs text-[#111111]">$50,000,000.00</p>
-            </div>
-            <div className="flex items-center gap-4 mb-4">
-              <p className="text-[#404040] font-work-sans-regular text-xs">
-               Deposit Time:
-              </p>
-              <p className="text-xs text-[#111111]">Instant</p>
-            </div>
-          </div>
-        </>
-      )}
+      </Row>
+    
     </React.Fragment>
   );
 };
--- src/components/shared/wrappers/deposit-wrapper.tsx
+++ src/components/shared/wrappers/deposit-wrapper.tsx
@@ -3,6 +3,7 @@ import radioChecked from "@/assets/svgs/radio-button-filled.svg";
 import radioUnChecked from "@/assets/svgs/radio-button.svg";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export const DepositWrapper = ({
@@ -19,11 +20,13 @@ export const DepositWrapper = ({
   setActiveIndex: (val: number) => void;
 }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const router = useRouter();
   return (
     <div className="relative w-full">
       <div
         onClick={() => {
           setActiveIndex(index);
+          router.push("/deposit/proceed");
         }}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
@@ -31,43 +34,28 @@ export const DepositWrapper = ({
           active
             ? "border-l-2 border-b-2 border-[#0DAE9480]"
             : "border-[#BEBEBE59] border"
-        }  w-full py-6  bg-white rounded-2xl p-4 flex items-center gap-2 cursor-pointer `}
+        }  w-full py-6  bg-white rounded-2xl p-4 flex items-start gap-2 cursor-pointer`}
       >
-        <Image src={active ? radioChecked : radioUnChecked} alt="" />
+        {/* <Image src={active ? radioChecked : radioUnChecked} alt="" /> */}
         <Image src={icon} alt="" />
-        <p className="lg:text-base text-sm font-work-sans-regular">{text}</p>
+        <div>
+          <p className="lg:text-base text-sm font-work-sans-regular">{text}</p>
+          <div className="mt-2">
+            <div className="flex items-center text-xs gap-4 mb-1">
+              <p className="text-[#707070]">Min Deposit:</p>
+              <p className="text-[#202020]">$10.00</p>
+            </div>
+            <div className="flex items-center text-xs gap-4 mb-1">
+              <p className="text-[#707070]">Max Deposit:</p>
+              <p className="text-[#202020]">$10.00</p>
+            </div>
+            <div className="flex items-center text-xs gap-4 mb-1">
+              <p className="text-[#707070]">Deposit Time:</p>
+              <p className="text-[#202020]">Instant - 12hrs</p>
+            </div>
+          </div>
+        </div>
       </div>
-
-      {isHovered && (
-        <motion.div
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 10 }}
-          transition={{ duration: 0.3 }}
-          className="bg-[#FFFFFF] w-full p-8 absolute -bottom-40 mt-2 rounded-lg shadow-md z-20"
-        >
-          <div className="flex items-center gap-8 mb-3">
-            <p className="text-[#404040] font-work-sans-regular">
-              Min Deposit:
-            </p>
-            <p className="font-work-sans-regular text-[#111111]">$10.00</p>
-          </div>
-          <div className="flex items-center gap-8 mb-3">
-            <p className="text-[#404040] font-work-sans-regular">
-              Max Deposit:
-            </p>
-            <p className="font-work-sans-regular text-[#111111]">
-              $50,000,000.00
-            </p>
-          </div>
-          <div className="flex items-center gap-8 mb-3">
-            <p className="text-[#404040] font-work-sans-regular">Commission:</p>
-            <p className="font-work-sans-regular text-[#111111]">
-              From $0.00 to $01.00
-            </p>
-          </div>
-        </motion.div>
-      )}
     </div>
   );
 };
