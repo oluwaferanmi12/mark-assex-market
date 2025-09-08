de535d8cd3bf681cb00b953161578ddbe2160e58
Mark <feranmijogbodo@gmail.com>
2025-09-08 08:23:53 +0100

    support redirect fix

--- src/components/ui/navbar/dashboard-side-nav.tsx
+++ src/components/ui/navbar/dashboard-side-nav.tsx
@@ -140,7 +140,7 @@ export const DashboardSideNav = ({
       activeIcon: dailyIcon,
       inactiveIcon: dailyIcon,
       clickAction: () => {},
-      slug: "bonus",
+      slug: "support",
     },
   ];
 
